d5195aa9a09cefeef05553b65bdc20a57b7cc28b
Mateo Borrazas <mborrazas@roku.com>
2023-06-23 14:26:31 -0300

    paginas

---
+++ app/musica/page.tsx
@@ -0,0 +1,88 @@
+'use client'
+export default function page() {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Pregunta</th>
+                <th>Respuesta Correcta</th>
+                <th>Respuesta Falsa 1</th>
+                <th>Respuesta Falsa 2</th>
+                <th>Respuesta Falsa 3</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>¿Quién es conocido como el "Rey del Pop"?</td>
+                <td>Michael Jackson</td>
+                <td>Elton John</td>
+                <td>Prince</td>
+                <td>Paul McCartney</td>
+            </tr>
+            <tr>
+                <td>¿Cuál es el álbum más vendido de todos los tiempos?</td>
+                <td>Thriller de Michael Jackson</td>
+                <td>Back In Black de AC/DC</td>
+                <td>Dark Side of The Moon de Pink Floyd</td>
+                <td>The Wall de Pink Floyd</td>
+            </tr>
+            <tr>
+                <td>¿Qué banda británica es famosa por canciones como "Bohemian Rhapsody" y "We Will Rock You"?</td>
+                <td>Queen</td>
+                <td>The Beatles</td>
+                <td>The Rolling Stones</td>
+                <td>Led Zeppelin</td>
+            </tr>
+            <tr>
+                <td>¿Quién lanzó el álbum titulado "1989" en el año 2014?</td>
+                <td>Taylor Swift</td>
+                <td>Katy Perry</td>
+                <td>Adele</td>
+                <td>Ariana Grande</td>
+            </tr>
+            <tr>
+                <td>¿Qué cantante es conocida por hits como "Like a Virgin" y "Material Girl"?</td>
+                <td>Madonna</td>
+                <td>Cyndi Lauper</td>
+                <td>Cher</td>
+                <td>Britney Spears</td>
+            </tr>
+            <tr>
+                <td>¿Qué cantante ganó el Grammy al álbum del año en 2022?</td>
+                <td>Harry Styles</td>
+                <td>Billie Eilish</td>
+                <td>Beyoncé</td>
+                <td>Dua Lipa</td>
+            </tr>
+            <tr>
+                <td>¿Qué banda es conocida por su álbum "The Dark Side of the Moon"?</td>
+                <td>Pink Floyd</td>
+                <td>The Beatles</td>
+                <td>Led Zeppelin</td>
+                <td>AC/DC</td>
+            </tr>
+            <tr>
+                <td>¿Quién es el cantante principal de la banda de rock U2?</td>
+                <td>Bono</td>
+                <td>Mick Jagger</td>
+                <td>Bruce Springsteen</td>
+                <td>David Bowie</td>
+            </tr>
+            <tr>
+                <td>¿Qué cantante pop es conocida por su álbum "Teenage Dream", que incluye cinco sencillos número uno en la lista Billboard Hot 100?</td>
+                <td>Katy Perry</td>
+                <td>Taylor Swift</td>
+                <td>Lady Gaga</td>
+                <td>Miley Cyrus</td>
+            </tr>
+            <tr>
+                <td>¿Qué banda es conocida por su álbum "Nevermind" que incluye el hit "Smells Like Teen Spirit"?</td>
+                <td>Nirvana</td>
+                <td>Pearl Jam</td>
+                <td>Radiohead</td>
+                <td>Metallica</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
---
+++ app/uruguay/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+export default function page() {
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>Pregunta</th>
+                <th>Respuesta Correcta</th>
+                <th>Respuesta Falsa 1</th>
+                <th>Respuesta Falsa 2</th>
+                <th>Respuesta Falsa 3</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>¿Quién fue el primer presidente de Uruguay después de la dictadura militar (1973-1985)?</td>
+                <td>Julio María Sanguinetti</td>
+                <td>Luis Alberto Lacalle</td>
+                <td>José Mujica</td>
+                <td>Jorge Batlle</td>
+            </tr>
+            <tr>
+                <td>¿Qué plebiscito significativo se realizó en Uruguay en 1980?</td>
+                <td>Se realizó un plebiscito para una nueva constitución propuesta por la dictadura militar, el cual fue
+                    rechazado por la mayoría.
+                </td>
+                <td>Plebiscito para legalizar el matrimonio igualitario</td>
+                <td>Plebiscito para la legalización de la marihuana</td>
+                <td>Plebiscito para la abolición de la pena de muerte</td>
+            </tr>
+            <tr>
+                <td>¿En qué año se legalizó el matrimonio igualitario en Uruguay?</td>
+                <td>2013</td>
+                <td>2010</td>
+                <td>2015</td>
+                <td>2008</td>
+            </tr>
+            <tr>
+                <td>¿Quién fue el presidente uruguayo que promovió la "ley de la marihuana", legalizando su producción y
+                    venta?
+                </td>
+                <td>José Mujica</td>
+                <td>Julio María Sanguinetti</td>
+                <td>Tabaré Vázquez</td>
+                <td>Luis Alberto Lacalle Pou</td>
+            </tr>
+            <tr>
+                <td>¿Cuándo y qué equipo uruguayo ganó por última vez la Copa Libertadores?</td>
+                <td>Club Nacional de Football, en 1988</td>
+                <td>Peñarol, en 1990</td>
+                <td>Club Nacional de Football, en 1995</td>
+                <td>Defensor Sporting, en 2000</td>
+            </tr>
+            <tr>
+                <td>¿En qué año se realizó la histórica huelga general contra la dictadura militar en Uruguay?</td>
+                <td>En 1973</td>
+                <td>En 1970</td>
+                <td>En 1976</td>
+                <td>En 1980</td>
+            </tr>
+            <tr>
+                <td>¿En qué año se estableció el Mercosur y qué países lo fundaron?</td>
+                <td>El Mercosur fue establecido en 1991 por Argentina, Brasil, Paraguay y Uruguay.</td>
+                <td>Mercosur fue establecido en 1995 por Argentina, Brasil, Chile y Uruguay.</td>
+                <td>Mercosur fue establecido en 1988 por Argentina, Brasil, Paraguay, Uruguay y Venezuela.</td>
+                <td>Mercosur fue establecido en 1993 por Argentina, Brasil, Uruguay, Paraguay y Bolivia.</td>
+            </tr>
+            <tr>
+                <td>¿Qué medidas destacadas tomó Uruguay durante la pandemia de COVID-19?</td>
+                <td>Uruguay se destacó por su manejo inicial de la pandemia, aplicando pruebas masivas y rastreo de
+                    contactos, aunque también enfrentó desafíos.
+                </td>
+                <td>Uruguay cerró completamente sus fronteras durante toda la pandemia.</td>
+                <td>Uruguay no tomó ninguna medida y dejó que la pandemia siguiera su curso natural.</td>
+                <td>Uruguay implementó una cuarentena obligatoria de largo plazo para todos los ciudadanos.</td>
+            </tr>
+            <tr>
+                <td>¿Qué importante reforma educativa tuvo lugar en Uruguay en los años 90?</td>
+                <td>La Reforma Rama, que transformó la educación secundaria con el objetivo de mejorar la calidad y
+                    equidad.
+                </td>
+                <td>Reforma de la educación universitaria para la autonomía total de las universidades.</td>
+                <td>Implementación de un sistema de educación bilingüe en todo el país.</td>
+                <td>Prohibición de los exámenes estandarizados a nivel nacional.</td>
+            </tr>
+            <tr>
+                <td>¿Cuál es el nombre del programa de inclusión digital que proporcionó laptops a los estudiantes de
+                    Uruguay?
+                </td>
+                <td>El Plan Ceibal</td>
+                <td>Programa One Laptop per Child</td>
+                <td>Proyecto Conectar Igualdad</td>
+                <td>Plan Ibirapitá</td>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
